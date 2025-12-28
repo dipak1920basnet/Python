797a3ba45661c645955f6c92170c76584fe372fa
dipak1920basnet <dipak1920basnet@gmail.com>
2025-12-28 08:44:18 +0545

    learned about print fuction and 'single line comment and multi line comment'

---
+++ 1_week/0/comment.py
@@ -0,0 +1,10 @@
+# Single line comments 
+
+"""
+Docstring for 0.comment
+Multi-line
+    Comments
+"""
+
+print("This program will print the multi-line comments")
+    print("This is indentation and will give error")
