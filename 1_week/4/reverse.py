6f4ccc143c79f71b6c37d0758c33f789e5b2b944
dipak1920basnet <dipak1920basnet@gmail.com>
2026-01-05 09:51:28 +0545

    practice for day 3 and 4

---
+++ 1_week/4/reverse.py
@@ -0,0 +1,11 @@
+def helloFUnction():
+    print("Hello")
+helloFUnction()
+
+def reverseexponentiation(n):
+        # code here
+    power = int((str(n)[::-1]))
+    # print(power)
+    return (n**power)
+
+print(reverseexponentiation(12))
